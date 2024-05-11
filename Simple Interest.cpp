b06c9c1773d64209b9c61b8505b8434ccc5a5f1a
sherali02 <133979320+sherali02@users.noreply.github.com>
2024-05-11 09:46:59 +0530

    Update Simple Interest.cpp
    https://www.geeksforgeeks.org/problems/simple-interest3457/1?page=1&category=Mathematical&difficulty=School&sortBy=submissions

--- Simple Interest.cpp
+++ Simple Interest.cpp
@@ -1,1 +1,15 @@
+// User function Template for C++
+class Solution {
+public:
+    double simpleInterest(int P, int R, int T) {
+        
+        double p = P;
+        return (p * T * R) / 100;
+    }
+}; 
+
+// This class provides a function to calculate simple interest.
+// It converts the principal amount to double for accurate calculation.
+// Then, it calculates simple interest using the formula: (P * T * R) / 100,
+// where P is the principal amount, T is the time period, and R is the rate of interest.
 
