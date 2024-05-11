89e5840b3b092d63a085450a1f9ee15ab794071d
sherali02 <133979320+sherali02@users.noreply.github.com>
2024-05-11 09:12:38 +0530

    Update Addition of two numbers.cpp
    https://www.geeksforgeeks.org/problems/addition-of-two-numbers0812/1?page=1&category=Mathematical&difficulty=School&sortBy=submissions

--- Addition of two numbers.cpp
+++ Addition of two numbers.cpp
@@ -1,1 +1,10 @@
+class Solution{   
+public:
+    int addition(int A, int B){
+        // code here 
+        return A+B;
+    }
+};
 
+// Adding the two integers A and B.
+// The result is returned.
