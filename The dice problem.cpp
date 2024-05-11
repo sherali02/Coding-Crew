c983ec2cc0a98d8e7adf7e4d68b869e1d7335675
sherali02 <133979320+sherali02@users.noreply.github.com>
2024-05-11 09:52:47 +0530

    Update The dice problem.cpp
    https://www.geeksforgeeks.org/problems/the-dice-problem2316/1?page=1&category=Mathematical&difficulty=School&sortBy=submissions

--- The dice problem.cpp
+++ The dice problem.cpp
@@ -1,1 +1,13 @@
+//User function Template for C++
+class Solution
+{
+public:
+    int oppositeFaceOfDice(int N)
+    {
+        return 7 - N;
+    }
+}; 
+
+// This class provides a function to find the opposite face of a dice given one face.
+// It calculates the opposite face by subtracting the given face from 7.
 
