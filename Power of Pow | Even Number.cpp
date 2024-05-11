d1378b67886af5045fe3c81a93307dccccb55ad5
sherali02 <133979320+sherali02@users.noreply.github.com>
2024-05-11 09:42:35 +0530

    Update Power of Pow | Even Number.cpp
    https://www.geeksforgeeks.org/problems/power-of-pow-even-number5440/1?page=2&category=Mathematical&difficulty=School&sortBy=submissions

--- Power of Pow | Even Number.cpp
+++ Power of Pow | Even Number.cpp
@@ -1,1 +1,14 @@
+class Solution
+{
+public:
+    long long int sum_of_square_evenNumbers(long long int n)
+    {
+        return n * (n + 1) * (2 * n + 1) * 2 / 3;
+    }
+}; 
+
+// This class provides a function to calculate the sum of squares of even numbers up to n.
+// It utilizes a formula that efficiently computes the sum of squares of even numbers.
+// The formula is n * (n + 1) * (2 * n + 1) * 2 / 3.
+// Finally, it returns the calculated sum of squares of even numbers.
 
