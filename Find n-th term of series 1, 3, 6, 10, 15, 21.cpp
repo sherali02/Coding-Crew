a77e24cb8ea3292ec1d414296d9cf0bacca24547
sherali02 <133979320+sherali02@users.noreply.github.com>
2024-05-11 09:24:41 +0530

    Update Find n-th term of series 1, 3, 6, 10, 15, 21.cpp
    https://www.geeksforgeeks.org/problems/find-n-th-term-of-series-1-3-6-10-15-215506/1?page=1&category=Mathematical&difficulty=School&sortBy=submissions

--- Find n-th term of series 1, 3, 6, 10, 15, 21.cpp
+++ Find n-th term of series 1, 3, 6, 10, 15, 21.cpp
@@ -1,1 +1,9 @@
 
+class Solution {
+public:
+    int findNthTerm(int N) {
+        return N * (N + 1) / 2;
+    }
+};
+
+// This function calculates the Nth term of a series using the formula N*(N+1)/2.
