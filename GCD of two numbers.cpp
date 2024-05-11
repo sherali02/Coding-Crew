5d7258a5a4095fa3d4af5d6780b4053d150c336e
sherali02 <133979320+sherali02@users.noreply.github.com>
2024-05-11 09:26:36 +0530

    Update GCD of two numbers.cpp
    https://www.geeksforgeeks.org/problems/gcd-of-two-numbers3459/1?page=1&category=Mathematical&difficulty=School&sortBy=submissions

--- GCD of two numbers.cpp
+++ GCD of two numbers.cpp
@@ -1,1 +1,16 @@
+class Solution {
+public:
+    int _gcd(int a, int b) {
+        if (b == 0) 
+            return a;
+        return _gcd(b, a % b);
+    }
 
+    int gcd(int a, int b) {
+        return _gcd(a, b);
+    }
+}; 
+
+// This class provides functions for calculating the greatest common divisor (GCD) of two integers.
+// The _gcd function recursively calculates the GCD using the Euclidean algorithm.
+// The gcd function is a wrapper function that calls _gcd with the given integers.
