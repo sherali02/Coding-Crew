0f0b8c522ec169fd20751b04283c45c5f798c118
sherali02 <133979320+sherali02@users.noreply.github.com>
2024-05-11 09:44:07 +0530

    Update Reverse digits.cpp
    https://www.geeksforgeeks.org/problems/reverse-digit0316/1?page=1&category=Mathematical&difficulty=School&sortBy=submissions

--- Reverse digits.cpp
+++ Reverse digits.cpp
@@ -1,1 +1,17 @@
+class Solution
+{
+public:
+    long long int reverse_digit(long long int n)
+    {
+        string s = to_string(n);
+        reverse(s.begin(), s.end());
+        long long int x = stoll(s);
+        return x;
+    }
+}; 
+
+// This class provides a function to reverse the digits of a given number.
+// It first converts the number to a string to facilitate reversal.
+// Then, it reverses the string representing the number using the reverse function.
+// Finally, it converts the reversed string back to a long long int and returns it.
 
