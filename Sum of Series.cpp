b8bd21d9b7b58ef9c7133795237f3d21b17693e9
sherali02 <133979320+sherali02@users.noreply.github.com>
2024-05-11 08:55:19 +0530

    Update Sum of Series.cpp
    https://www.geeksforgeeks.org/problems/sum-of-series2811/1?page=1&category=Mathematical&difficulty=School&sortBy=submissions

--- Sum of Series.cpp
+++ Sum of Series.cpp
@@ -7,3 +7,12 @@ class Solution {
         return s;
     }
 };
+
+//Explanation:
+
+// To find the sum of the first 'n' natural numbers, 
+// we utilize the mathematical formula: sum = n * (n + 1) / 2
+// However, as the input data is of type 'int', 
+// the evaluation of this formula might result in integer overflow.
+// To mitigate this risk, we typecast 'n' to 'long long' 
+// before performing the calculation.
