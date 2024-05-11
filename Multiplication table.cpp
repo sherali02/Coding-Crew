9f1225fe38422f464d18a927d432263c3d4f2884
sherali02 <133979320+sherali02@users.noreply.github.com>
2024-05-11 09:38:09 +0530

    Update Multiplication table.cpp
    https://www.geeksforgeeks.org/problems/print-table0303/1?page=1&category=Mathematical&difficulty=School&sortBy=submissions

--- Multiplication table.cpp
+++ Multiplication table.cpp
@@ -1,1 +1,19 @@
+//User function Template for C++
+class Solution
+{
+public:
+    vector<int> getTable(int N)
+    {
+        vector<int> a;
+        for(int i=1;i<=10;i++){
+            a.push_back(N*i);
+        }
+        return a;
+    }
+}; 
+
+// This function generates a table of multiples of N from 1 to 10.
+// It initializes an empty vector to store the table values.
+// Then, it iterates from 1 to 10, calculates the multiples of N for each iteration, and stores them in the vector.
+// Finally, it returns the vector containing the generated table.
 
