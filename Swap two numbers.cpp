baf5ced2bb865db93175387c0429e60690898e49
sherali02 <133979320+sherali02@users.noreply.github.com>
2024-05-11 09:51:46 +0530

    Update Swap two numbers.cpp
    https://www.geeksforgeeks.org/problems/swap-two-numbers3844/1?page=1&category=Mathematical&difficulty=School&sortBy=submissions

--- Swap two numbers.cpp
+++ Swap two numbers.cpp
@@ -1,1 +1,12 @@
 
+//User function Template for C++
+class Solution{   
+public:
+    pair<int, int> get(int a, int b){
+        swap(a, b);
+        return {a, b};
+    }
+}; 
+
+// This class provides a function to swap the values of two integers and return them as a pair.
+// It swaps the values of a and b using the swap function and returns a pair containing the swapped values.
