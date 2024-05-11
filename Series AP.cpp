efdfc37561829388357e1b3a390f2ff63bda080f
sherali02 <133979320+sherali02@users.noreply.github.com>
2024-05-11 09:45:28 +0530

    Update Series AP.cpp
    https://www.geeksforgeeks.org/problems/series-ap5310/1?page=1&category=Mathematical&difficulty=School&sortBy=submissions

--- Series AP.cpp
+++ Series AP.cpp
@@ -1,1 +1,13 @@
+// User function template for C++
+class Solution {
+public:
+    int nthTermOfAP(int a1, int a2, int n) {
+        a2 = a2 - a1;
+        return a1 + (n - 1) * a2;
+    }
+}; 
+
+// This class provides a function to calculate the nth term of an arithmetic progression (AP).
+// It calculates the common difference between consecutive terms and then uses the formula: a1 + (n - 1) * d
+// to find the nth term of the AP, where a1 is the first term, n is the position of the term, and d is the common difference.
 
