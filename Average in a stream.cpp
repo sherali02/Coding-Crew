ba938abe84489806a5db910f80d4b01645e136cf
sherali02 <133979320+sherali02@users.noreply.github.com>
2024-05-11 09:15:45 +0530

    Update Average in a stream.cpp
    https://www.geeksforgeeks.org/problems/average4856/1?page=1&category=Mathematical&difficulty=School&sortBy=submissions

--- Average in a stream.cpp
+++ Average in a stream.cpp
@@ -1,1 +1,20 @@
+class Solution{
+public:	
+	vector<float> streamAvg(int arr[], int n) {
+	    // code here
+	    float s=0;
+	    vector<float>ans;
+	   // float i=1;
+	    for(int i=0;i<n;i++){
+	        s+=arr[i];
+	        ans.push_back(s/(i+1));
+	    }
+	    return ans;
+	}
+};
 
+
+// To compute the average of elements in a streaming fashion,
+// we maintain a running sum of the elements seen so far.
+// For each element, we add it to the running sum and compute the average.
+// The computed average is stored in a vector and returned at the end.
