f422fad5d6e1460bbfca0a03e40d78ee455f0be4
sherali02 <133979320+sherali02@users.noreply.github.com>
2024-05-11 09:41:26 +0530

    Update Odd or even.cpp
    https://www.geeksforgeeks.org/problems/odd-or-even3618/1?page=1&category=Mathematical&difficulty=School&sortBy=submissions

--- Odd or even.cpp
+++ Odd or even.cpp
@@ -1,1 +1,15 @@
 
+class Solution{   
+public:
+    string oddEven(int N){
+        if(N & 1)
+            return "odd";
+        else
+            return "even";
+    }
+}; 
+
+// This class provides a function to determine whether a given number is odd or even.
+// It uses bitwise AND operation with 1 to check if the least significant bit of the number is set,
+// which indicates whether the number is odd or even.
+// If the result of the operation is non-zero, the number is odd; otherwise, it's even.
