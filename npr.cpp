7637e9477d5bdd32d39a020c1a378b5aeffb00df
sherali02 <133979320+sherali02@users.noreply.github.com>
2024-05-11 09:54:12 +0530

    Update npr.cpp
    https://www.geeksforgeeks.org/problems/npr4253/1?page=1&category=Mathematical&difficulty=School&sortBy=submissions

--- npr.cpp
+++ npr.cpp
@@ -1,1 +1,15 @@
+// User function Template for C++
+class Solution{
+public:
+    long long nPr(int n, int r){
+        long long ans=1;
+        for(long long int i=n;i>n-r;i--){
+            ans*=i;
+        }
+        return ans;
+    }
+}; 
+
+// This class provides a function to calculate the permutation of n items taken r at a time.
+// It iteratively multiplies numbers from n down to (n - r + 1) to compute the permutation value.
 
